a4767634cbbec8c051d90f980dc493a6a5d04843
Niv Sardi <xaiki@evilgiggle.com>
2019-12-20 02:40:39 -0300

    python: remove debug
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- python/cloud_server.py
+++ python/cloud_server.py
@@ -20,12 +20,10 @@ def hello_world():
 @app.route('/img/<string:hash>', methods=['GET', 'POST'])
 def hash_image(hash):
     filename = f"{basepath}/imgs/{hash}.png"
-    print("got request for", hash)
     try:
         return send_file(filename, mimetype="image/png")
     except:
         text = request.get_data().decode("utf-8")
-        print("processing for", text)
         wc.generate(text).to_file(filename)
         return send_file(filename, mimetype="image/png")
 
