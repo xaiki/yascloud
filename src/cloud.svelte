483b05310cd055557a3af45c0ece4337c36409a6
Niv Sardi <xaiki@evilgiggle.com>
2019-12-20 02:40:49 -0300

    full size image
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/cloud.svelte
+++ src/cloud.svelte
@@ -133,13 +133,18 @@
      overflow: hidden;
      position: relative;
      width: 100%;
-     
+
      & .messages-content {
          position: absolute;
          top: 0;
          left: 0;
-         height: 101%;
+         height: 100%;
          width: 100%;
+     }
+
+     & img {
+         width: 100%;
+         height: 100%;
      }
  }
 
